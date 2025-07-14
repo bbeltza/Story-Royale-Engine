13674b5f8526289118973d78689b623bcb3ef8ef
bbeltza <amigorayan2010@outlook.es>
2025-07-14 10:26:37 +0200

    Moved TODO section into the CMakeLists

--- include/Engine.h
+++ include/Engine.h
@@ -39,29 +39,3 @@ private:
 
 extern EngineClass* Engine;
 
-// TODO:
-/*
-
-!!: Done!
-!: Working on!
-
-
-- Window running; !!
-- Window updating/filling the background with a color; !!
-- Instancing/Scenes; !!
-- Collision detection; !!
-- Linking with ImGui (maybe before uhhh); *maybe after
-- Rendering instances/entities; !!
-- Input handling; !!
-- Components; !!
-
-- User interface layers; !
-
-- Rendering text; !
-- Rendering textures;
-
-- Audio;
-
-- Work on the game!;
-
-*/
