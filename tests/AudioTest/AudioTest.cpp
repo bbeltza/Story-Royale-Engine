41492be477552271a0991f750b0b59e67b286b87
bbeltza <amigorayan2010@outlook.es>
2025-09-11 20:29:22 +0200

    Added a TRUE test to check both channels and a piece of song that might be in Story Royale...

--- tests/AudioTest/AudioTest.cpp
+++ tests/AudioTest/AudioTest.cpp
@@ -1,16 +1,30 @@
 #include <Engine.h>
 
-Audio test1, test2;
+Audio stereotest;
+Audio test1, test2, test3;
 
 void Initialize()
 {
     printf("Hello world!\n");
 
+    AudioData& stereotest_data = Engine->AudioDevice.LoadAudio("res://stereotest.wav");
     AudioData& test1_data = Engine->AudioDevice.LoadAudio("res://test1.wav");
     AudioData& test2_data = Engine->AudioDevice.LoadAudio("res://test2.ogg");
+    AudioData& test3_data = Engine->AudioDevice.LoadAudio("res://test3.ogg");
 
+    stereotest.Attach(stereotest_data);
     test1.Attach(test1_data);
     test2.Attach(test2_data);
+    test3.Attach(test3_data);
 
-    Engine->AudioDevice.PlayAudio(test2);
+    test3.Info.fade_in = 1;
+    test3.Info.looped = true;
+    test3.Info.volume = 2;
+    test3.Info.speed = 1;
+
+    stereotest.Info.looped = true;
+
+    Engine->AudioDevice.PlayAudio(test3);
+
+    //Engine->Window.hide();
 }
