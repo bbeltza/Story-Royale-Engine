037a2aec0b821e979d635d632863a0ebcc986a31
bbeltza <amigorayan2010@outlook.es>
2025-09-25 15:35:01 +0200

    Ok now it does include

---
+++ include/Datatypes/Color.h
@@ -0,0 +1,1 @@
+#include "C/Color.h"
---
+++ include/Datatypes/TimeStamp.h
@@ -0,0 +1,4 @@
+#pragma once
+
+// The typed used for the duration of the timers. It is set to float for now, but might be changed to a double later
+typedef float TimeStamp;
