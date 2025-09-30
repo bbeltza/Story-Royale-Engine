27c8d822fa662728396ebeedda304ee4ad77b72b
bbeltza <amigorayan2010@outlook.es>
2025-09-30 12:22:04 +0200

    A util that was in StoryRoyale's source code: Creates a null terminated array

--- include/utils.h
+++ include/utils.h
@@ -12,3 +12,10 @@
 #ifndef mmin
 #define mmin(x, y) ( x > y ? y : x)
 #endif
+
+/* 
+Creates a null terminated array, of anything, takes any argument to put into an array in which will add NULL at the end, to be able to iterate without having to speficy a size.
+
+It's discouraged to use it with numbers as numbers can be NULL too
+*/
+#define array(...) {__VA_ARGS__, NULL}
