31dbd6e9b4053ebe1c0be3ba4b507003186dcff2
bbeltza <amigorayan2010@outlook.es>
2025-12-04 12:54:29 +0100

    Organized gui and world source trees

--- src/gamelayer/components/gui/AspectRatio.cpp
+++
@@ -1,6 +0,0 @@
-#include "Game/GuiComponents/AspectRatio.hpp"
-
-void GuiComponents::AspectRatio::process_size(Game::GuiContainer* obj)
-{
-    getAbsolute(obj)->Size.X = getAbsolute(obj)->Size.Y * ratio;
-}
