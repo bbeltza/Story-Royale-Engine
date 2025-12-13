2c2d54b120c003a55094fbc2cf20abb9276d1526
bbeltza <amigorayan2010@outlook.es>
2025-12-13 16:42:40 +0100

    I missed the Vector2 test, so I added some comments to describe the ideas! For the examples, uh... They're still abandoned...

---
+++ tests/vec2/init.cpp
@@ -0,0 +1,48 @@
+#ifdef NDEBUG
+    #undef NDEBUG // Tests should be interpreted as in debug mode, even on release
+#endif
+#include <Engine.hpp>
+#include <datatypes/vector.hpp>
+
+#define TEST_FAIL "Test failed (static assertion)"
+
+void Game::Initialize()
+{
+    using namespace sre;
+    // Every single test is independent and thus will have its own scope
+
+    // Constructors
+        // Default
+        {
+            vec2i vec2test;
+            static_assert(std::is_same<vec2i::type, int>::value, TEST_FAIL);
+            assert(vec2test.x == 0 && vec2test.y == 0);
+        }
+        // X and Y
+
+        // Scalar
+        // Copy
+
+        // Conversion
+    
+    // Operations
+        // Function operations
+        // Type agnostic function operations
+
+        // Function operation getters
+        // + Type checking
+
+        // Overloaded operators
+        // + Type checking
+    
+    // Math operations (e.g. abs & dot)
+    
+    // Equality overloaded operators ( == and != )
+
+    // Constant expression checks
+    {
+        static constexpr vec2i constexprtest{100, 100};
+        static_assert(constexprtest.x == 100 && constexprtest.y == 100, TEST_FAIL);
+    }
+    exit(0);
+}
