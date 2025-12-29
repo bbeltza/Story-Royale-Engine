01c03598852328112f76de09b84cbc36d2b31ed2
bbeltza <amigorayan2010@outlook.es>
2025-12-29 10:11:01 +0100

    Just adding these sources and!

---
+++ src/presets/gui/components/image.cpp
@@ -0,0 +1,23 @@
+#include <GUI/Components/image.hpp>
+#include <GUI/Components/transform.hpp>
+#include "Engine.hpp"
+
+using namespace sreGUI;
+
+void Image::fit(Transform& transform)
+{
+    if (!texture)
+    {
+        WARN("Calling Image::fit on NULL image");
+        return;
+    }
+    sre::vec2i size = texture->size();
+    transform.size = sre::udim2::fromoffset(sre::vec2ut{size});
+}
+
+void Image::on_render(const sre::rect2Dut &dimensions)
+{
+    if (!texture) return;
+
+    Display::DrawTexture(*texture, dimensions, modulate, sre::vec2f::ZERO, DISPLAY_DONT_CENTER);
+}
