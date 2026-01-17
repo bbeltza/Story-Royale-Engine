5f5034cae355def274d7baec44ba1e7c3320188a
bbeltza <amigorayan2010@outlook.es>
2026-01-17 11:09:26 +0100

    Add thread.cpp

---
+++ src/base/thread.cpp
@@ -0,0 +1,20 @@
+#include <Base/Thread.hpp>
+#include <utils/logging.h>
+
+sre::Thread::result_type sre::Thread::join()
+{
+    int success;
+    result_type ret = sre_threadjoin(m_id, &success);
+
+    if (!success)
+        ERROR("Thread::join() failed");
+
+    m_id = SRE_INVALIDTHREAD;
+    return ret;
+}
+
+void sre::Thread::detach()
+{
+    sre_threaddetach(m_id);
+    m_id = SRE_INVALIDTHREAD;
+}
