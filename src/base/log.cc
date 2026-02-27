b1f1590903791b304865d462ce44638ee1e93ea3
bbeltza <amigorayan2010@outlook.es>
2026-02-27 21:11:57 +0100

    OKAYY I fixed it now! `va` was being modified during the first pass so the second pass had `va` without the passed arguments / they were garbage values I got no idea how or why MSVC did function... Maybe it copied the va_list but at this point it's literally silent diabolical undefined implementation-speficic behavior!

--- src/base/log.cc
+++ src/base/log.cc
@@ -186,7 +186,9 @@ int sre_logEx(int type, int category, const char* fmt, va_list va)
     auto& instance = log_instance();
     std::lock_guard<std::recursive_mutex> guard{instance.mutex};
 
-    int len = vsnprintf(NULL, 0, fmt, va) + 1;
+    va_list cpy;
+    va_copy(cpy, va);
+    int len = vsnprintf(NULL, 0, fmt, cpy) + 1;
     instance.msg_queue.emplace_back(
         type,
         category,
